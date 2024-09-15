43a2cfca6b4d2ed635429a007c52b8030ba14503
Emiliaceb <emiceballoss24@gmail.com>
2024-09-15 19:32:38 -0300

    maquetado del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Formulario
+ from './components/Formulario';
 function App() {
 
 
   return (
     <>
-    <h1>Informacion del clima de tu ubicacion</h1>
+    <h1 className='text-center'>Informacion del clima de tu ubicacion</h1>
+     <section className='container'>
+      <div></div>
+      <div>
       <Formulario></Formulario>
+      </div>
+     </section>
     </>
   )
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Formulario = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <Form className="w-25">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Ubicacion</Form.Label>
+          <Form.Control type="text" placeholder="Ej: San Miguel de Tucuman" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Pais</Form.Label>
+          <Form.Control type="text" placeholder="Ej: Argentina" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Consultar clima
+        </Button>
+      </Form>
+    </>
+  );
 };
 
 export default Formulario;

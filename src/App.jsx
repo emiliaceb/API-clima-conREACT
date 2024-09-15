322ebdc6de7208ed53be999bdbd7f12f6967b1b9
Emiliaceb <emiceballoss24@gmail.com>
2024-09-15 19:22:39 -0300

    estructura del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Informacion del clima de tu ubicacion</h1>
+      <Formulario></Formulario>
+    </>
+  )
+}
+
+export default App
